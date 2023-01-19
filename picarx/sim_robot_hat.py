41a8b681576cd9cb4b5639085d5f8daf72c384ea
patilunmesh <patilunmeshrao@gmail.com>
2023-01-19 13:11:38 -0800

    added hardware proxy files for simulation

---
+++ picarx/sim_robot_hat.py
@@ -0,0 +1,551 @@
+# -*- coding: utf-8 -*-
+import logging
+import time
+
+
+class _Basic_class(object):
+    _class_name = '_Basic_class'
+    DEBUG_LEVELS = {'debug': logging.DEBUG,
+                    'info': logging.INFO,
+                    'warning': logging.WARNING,
+                    'error': logging.ERROR,
+                    'critical': logging.CRITICAL,
+                    }
+    DEBUG_NAMES = ['critical', 'error', 'warning', 'info', 'debug']
+
+    def __init__(self):
+        self._debug_level = 0
+        self.logger = logging.getLogger(self._class_name)
+        self.ch = logging.StreamHandler()
+        form = "%(asctime)s	[%(levelname)s]	%(message)s"
+        self.formatter = logging.Formatter(form)
+        self.ch.setFormatter(self.formatter)
+        self.logger.addHandler(self.ch)
+        self._debug = self.logger.debug
+        self._info = self.logger.info
+        self._warning = self.logger.warning
+        self._error = self.logger.error
+        self._critical = self.logger.critical
+
+    @property
+    def debug(self):
+        return self._debug_level
+
+    @debug.setter
+    def debug(self, debug):
+        if debug in range(5):
+            self._debug_level = self.DEBUG_NAMES[debug]
+        elif debug in self.DEBUG_NAMES:
+            self._debug_level = debug
+        else:
+            raise ValueError(
+                'Debug value must be 0(critical), 1(error), 2(warning), 3(info) or 4(debug), not \"{0}\".'.format(debug))
+        self.logger.setLevel(self.DEBUG_LEVELS[self._debug_level])
+        self.ch.setLevel(self.DEBUG_LEVELS[self._debug_level])
+        self._debug('Set logging level to [%s]' % self._debug_level)
+
+    def run_command(self, cmd):
+        import subprocess
+        p = subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = p.stdout.read().decode('utf-8')
+        status = p.poll()
+        # print(result)
+        # print(status)
+        return status, result
+
+    def map(self, x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+
+class I2C(_Basic_class):
+    MASTER = 0
+    SLAVE = 1
+    RETRY = 5
+
+    def __init__(self, *args, **kargs):
+        super().__init__()
+        self._bus = 1
+
+    def _i2c_write_byte(self, addr, data):
+        self._debug(
+            "_i2c_write_byte: [0x{:02X}] [0x{:02X}]".format(addr, data))
+
+    def _i2c_write_byte_data(self, addr, reg, data):
+        self._debug("_i2c_write_byte_data: [0x{:02X}] [0x{:02X}] [0x{:02X}]".format(
+            addr, reg, data))
+
+    def _i2c_write_word_data(self, addr, reg, data):
+        self._debug("_i2c_write_word_data: [0x{:02X}] [0x{:02X}] [0x{:04X}]".format(
+            addr, reg, data))
+
+    def _i2c_write_i2c_block_data(self, addr, reg, data):
+        self._debug(
+            "_i2c_write_i2c_block_data: [0x{:02X}] [0x{:02X}] {}".format(addr, reg, data))
+
+    def _i2c_read_byte(self, addr):
+        self._debug("_i2c_read_byte: [0x{:02X}]".format(addr))
+
+    def _i2c_read_i2c_block_data(self, addr, reg, num):
+        self._debug(
+            "_i2c_read_i2c_block_data: [0x{:02X}] [0x{:02X}] [{}]".format(addr, reg, num))
+
+    def is_ready(self, addr):
+        addresses = self.scan()
+        if addr in addresses:
+            return True
+        else:
+            return False
+
+    def scan(self):
+        cmd = "i2cdetect -y %s" % self._bus
+        _, output = self.run_command(cmd)
+
+        outputs = output.split('\n')[1:]
+        self._debug("outputs")
+        addresses = []
+        for tmp_addresses in outputs:
+            if tmp_addresses == "":
+                continue
+            tmp_addresses = tmp_addresses.split(':')[1]
+            tmp_addresses = tmp_addresses.strip().split(' ')
+            for address in tmp_addresses:
+                if address != '--':
+                    addresses.append(int(address, 16))
+        self._debug("Conneceted i2c device: %s" % addresses)
+        return addresses
+
+    def send(self, send, addr, timeout=0):
+        if isinstance(send, bytearray):
+            data_all = list(send)
+        elif isinstance(send, int):
+            data_all = []
+            d = "{:X}".format(send)
+            d = "{}{}".format("0" if len(d) % 2 == 1 else "", d)
+            for i in range(len(d)-2, -1, -2):
+                tmp = int(d[i:i+2], 16)
+                data_all.append(tmp)
+            data_all.reverse()
+        elif isinstance(send, list):
+            data_all = send
+        else:
+            raise ValueError(
+                "send data must be int, list, or bytearray, not {}".format(type(send)))
+
+        if len(data_all) == 1:
+            data = data_all[0]
+            self._i2c_write_byte(addr, data)
+        elif len(data_all) == 2:
+            reg = data_all[0]
+            data = data_all[1]
+            self._i2c_write_byte_data(addr, reg, data)
+        elif len(data_all) == 3:
+            reg = data_all[0]
+            data = (data_all[2] << 8) + data_all[1]
+            self._i2c_write_word_data(addr, reg, data)
+        else:
+            reg = data_all[0]
+            data = list(data_all[1:])
+            self._i2c_write_i2c_block_data(addr, reg, data)
+
+    def recv(self, recv, addr=0x00, timeout=0):
+        if isinstance(recv, int):
+            result = bytearray(recv)
+        elif isinstance(recv, bytearray):
+            result = recv
+        else:
+            return False
+        for i in range(len(result)):
+            result[i] = self._i2c_read_byte(addr)
+        return result
+
+    def mem_write(self, data, addr, memaddr, timeout=5000, addr_size=8):  # memaddr match to chn
+        if isinstance(data, bytearray):
+            data_all = list(data)
+        elif isinstance(data, list):
+            data_all = data
+        elif isinstance(data, int):
+            data_all = []
+            data = "%x" % data
+            if len(data) % 2 == 1:
+                data = "0" + data
+            # print(data)
+            for i in range(0, len(data), 2):
+                # print(data[i:i+2])
+                data_all.append(int(data[i:i+2], 16))
+        else:
+            raise ValueError(
+                "memery write require arguement of bytearray, list, int less than 0xFF")
+        # print(data_all)
+        self._i2c_write_i2c_block_data(addr, memaddr, data_all)
+
+    def mem_read(self, data, addr, memaddr, timeout=5000, addr_size=8):     # 读取数据
+        if isinstance(data, int):
+            num = data
+        elif isinstance(data, bytearray):
+            num = len(data)
+        else:
+            return False
+        result = bytearray(self._i2c_read_i2c_block_data(addr, memaddr, num))
+        return result
+
+    def readfrom_mem_into(self, addr, memaddr, buf):
+        buf = self.mem_read(len(buf), addr, memaddr)
+        return buf
+
+    def writeto_mem(self, addr, memaddr, data):
+        self.mem_write(data, addr, memaddr)
+
+
+
+class ADC(I2C):
+    ADDR = 0x14
+
+    def __init__(self, chn):
+        super().__init__()
+        if isinstance(chn, str):
+            if chn.startswith("A"):
+                chn = int(chn[1:])
+            else:
+                raise ValueError(
+                    "ADC channel should be between [A0, A7], not {0}".format(chn))
+        if chn < 0 or chn > 7:
+            self._error('Incorrect channel range')
+        chn = 7 - chn
+        self.chn = chn | 0x10
+        self.reg = 0x40 + self.chn
+
+    def read(self):
+        self._debug("Write 0x%02X to 0x%02X" % (self.chn, self.ADDR))
+        # self.send([self.chn, 0, 0], self.ADDR)
+
+        self._debug("Read from 0x%02X" % (self.ADDR))
+        value_h = 0   # self.recv(1, self.ADDR)[0]
+
+        self._debug("Read from 0x%02X" % (self.ADDR))
+        value_l = 0   # self.recv(1, self.ADDR)[0]
+
+        value = (value_h << 8) + value_l
+        self._debug("Read value: %s" % value)
+        return value
+
+    def read_voltage(self):
+        return self.read*3.3/4095
+
+
+class Pin(_Basic_class):
+    PULL_NONE = None
+    _dict = {
+        "BOARD_TYPE": 12,
+    }
+
+    _dict_1 = {
+        "D0":  17,
+        "D1":  18,
+        "D2":  27,
+        "D3":  22,
+        "D4":  23,
+        "D5":  24,
+        "D6":  25,
+        "D7":  4,
+        "D8":  5,
+        "D9":  6,
+        "D10": 12,
+        "D11": 13,
+        "D12": 19,
+        "D13": 16,
+        "D14": 26,
+        "D15": 20,
+        "D16": 21,
+        "SW":  19,
+        "LED": 26,
+        "BOARD_TYPE": 12,
+        "RST": 16,
+        "BLEINT": 13,
+        "BLERST": 20,
+        "MCURST": 21,
+    }
+
+    _dict_2 = {
+        "D0":  17,
+        "D1":   4,  # Changed
+        "D2":  27,
+        "D3":  22,
+        "D4":  23,
+        "D5":  24,
+        "D6":  25,  # Removed
+        "D7":   4,  # Removed
+        "D8":   5,  # Removed
+        "D9":   6,
+        "D10": 12,
+        "D11": 13,
+        "D12": 19,
+        "D13": 16,
+        "D14": 26,
+        "D15": 20,
+        "D16": 21,
+        "SW":  25,  # Changed
+        "LED": 26,
+        "BOARD_TYPE": 12,
+        "RST": 16,
+        "BLEINT": 13,
+        "BLERST": 20,
+        "MCURST":  5,  # Changed
+    }
+
+    def __init__(self, *value):
+        super().__init__()
+        self.check_board_type()
+
+        if len(value) > 0:
+            pin = value[0]
+        if len(value) > 1:
+            mode = value[1]
+        else:
+            mode = None
+        if len(value) > 2:
+            setup = value[2]
+        else:
+            setup = None
+        if isinstance(pin, str):
+            try:
+                self._board_name = pin
+                self._pin = self.dict()[pin]
+            except Exception as e:
+                print(e)
+                self._error('Pin should be in %s, not %s' %
+                            (self._dict.keys(), pin))
+        elif isinstance(pin, int):
+            self._pin = pin
+        else:
+            self._error('Pin should be in %s, not %s' %
+                        (self._dict.keys(), pin))
+        self._value = 0
+        self.init(mode, pull=setup)
+        self._info("Pin init finished.")
+
+    def check_board_type(self):
+        type_pin = self.dict()["BOARD_TYPE"]
+        if True:
+            self._dict = self._dict_1
+        else:
+            self._dict = self._dict_2
+
+    def init(self, mode, pull=PULL_NONE):
+        self._pull = pull
+        self._mode = mode
+
+    def dict(self, *_dict):
+        if len(_dict) == 0:
+            return self._dict
+        else:
+            if isinstance(_dict, dict):
+                self._dict = _dict
+            else:
+                self._error(
+                    'argument should be a pin dictionary like {"my pin": ezblock.Pin.cpu.GPIO17}, not %s' % _dict)
+
+    def __call__(self, value):
+        return self.value(value)
+
+    def value(self, *value):
+        return 0
+
+    def on(self):
+        return self.value(1)
+
+    def off(self):
+        return self.value(0)
+
+    def high(self):
+        return self.on()
+
+    def low(self):
+        return self.off()
+
+    def mode(self, *value):
+        if len(value) == 0:
+            return (self._mode, self._pull)
+        else:
+            self._mode = value[0]
+
+    def pull(self, *value):
+        return self._pull
+
+    def irq(self, handler=None, trigger=None, bouncetime=200):
+        self.mode(self.IN)
+
+    def name(self):
+        return "GPIO%s" % self._pin
+
+    def names(self):
+        return [self.name, self._board_name]
+
+    class cpu(object):
+        GPIO17 = 17
+        GPIO18 = 18
+        GPIO27 = 27
+        GPIO22 = 22
+        GPIO23 = 23
+        GPIO24 = 24
+        GPIO25 = 25
+        GPIO26 = 26
+        GPIO4 = 4
+        GPIO5 = 5
+        GPIO6 = 6
+        GPIO12 = 12
+        GPIO13 = 13
+        GPIO19 = 19
+        GPIO16 = 16
+        GPIO26 = 26
+        GPIO20 = 20
+        GPIO21 = 21
+
+
+timer = [
+    {
+        "arr": 0
+    }
+] * 4
+
+
+class PWM():
+    REG_CHN = 0x20
+    REG_FRE = 0x30
+    REG_PSC = 0x40
+    REG_ARR = 0x44
+
+    ADDR = 0x14
+
+    CLOCK = 72000000
+
+    def __init__(self, channel, debug="critical"):
+        super().__init__()
+        if isinstance(channel, str):
+            if channel.startswith("P"):
+                channel = int(channel[1:])
+            else:
+                raise ValueError(
+                    "PWM channel should be between [P1, P14], not {0}".format(channel))
+
+        self.debug = debug
+
+    def i2c_write(self, reg, value):
+        pass
+
+    def freq(self, *freq):
+        pass
+
+    def prescaler(self, *prescaler):
+        pass
+
+    def period(self, *arr):
+        pass
+
+    def pulse_width(self, *pulse_width):
+        pass
+
+    def pulse_width_percent(self, *pulse_width_percent):
+        pass
+
+class Ultrasonic:
+    """
+    stub for ezblocks/ultrasonic
+    """
+    def __init__(self, pin1: Pin, pin2: Pin) -> None:
+        pass
+
+    def read(self):
+        return 0
+
+class Servo(_Basic_class):
+    MAX_PW = 2500
+    MIN_PW = 500
+    _freq = 50
+
+    def __init__(self, pwm):
+        super().__init__()
+
+    # angle ranges -90 to 90 degrees
+    def angle(self, angle):
+        pass
+
+class Grayscale_Module(object):
+    def __init__(self, pin0, pin1, pin2, reference=1000):
+        self.chn_0 = ADC(pin0)
+        self.chn_1 = ADC(pin1)
+        self.chn_2 = ADC(pin2)
+        self.reference = reference
+
+    def get_line_status(self,fl_list):
+
+        return 0
+
+    def get_grayscale_data(self):
+        adc_value_list = []
+        adc_value_list.append(self.chn_0.read())
+        adc_value_list.append(self.chn_1.read())
+        adc_value_list.append(self.chn_2.read())
+        return [0, 0, 0]
+
+
+class Ultrasonic():
+    def __init__(self, trig, echo, timeout=0.02):
+        self.trig = trig
+        self.echo = echo
+        self.timeout = timeout
+
+    def _read(self):
+        self.trig.low()
+        time.sleep(0.01)
+        self.trig.high()
+        time.sleep(0.00001)
+        self.trig.low()
+        pulse_end = 0
+        pulse_start = 0
+        timeout_start = time.time()
+        while self.echo.value()==0:
+            pulse_start = time.time()
+            if pulse_start - timeout_start > self.timeout:
+                return -1
+        while self.echo.value()==1:
+            pulse_end = time.time()
+            if pulse_end - timeout_start > self.timeout:
+                return -1
+        during = pulse_end - pulse_start
+        cm = round(during * 340 / 2 * 100, 2)
+        return cm
+
+    def read(self, times=10):
+        return -1
+
+
+def test(): #pwm
+    pass
+
+
+def test2p(): #pwm
+    pass
+
+def __reset_mcu__():
+    print ("resetting mcu.")
+
+def reset_mcu():
+    print ("resetting mcu.")
+
+
+def testi2c(): #i2c
+    import time
+    adc = ADC(0)
+    while True:
+        print(adc.read())
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    test()
+    testi2c()
+    test2()
+    reset_mcu()
+    __reset_mcu__()
