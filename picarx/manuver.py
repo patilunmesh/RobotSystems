b4730e98ee8c9b90a235d3b02d4ac0205034f9b4
patilunmesh <patilunmeshrao@gmail.com>
2023-01-24 12:22:12 -0800

    indent fix

--- picarx/manuver.py
+++ picarx/manuver.py
@@ -15,83 +15,80 @@ class Manuver():
 
 	def Moveit(self, order = "forward"):
 		if order == "forward":
-            self.picar.always_forward(75)
-            self.delay_stop()
-        elif order == "backward":
-            self.picar.always_backward(75)
-            self.delay_stop()
-        elif order == "forward left":
-            self.picar.set_dir_servo_angle(-30)
-            self.picar.forward(75)
-            self.delay_stop()
-        elif order == "forward right":
-            self.picar.set_dir_servo_angle(30)
-            self.picar.forward(75)
-            self.delay_stop()
-        elif order == "backward left":
-            self.picar.set_dir_servo_angle(-30)
-            self.picar.backward(75)
-            self.delay_stop()
-        elif order == "backward right":
-            self.picar.set_dir_servo_angle(30)
-            self.picar.backward(75)
-            self.delay_stop()
-        elif order == 'stop':
-            self.picar.stop()
-		
+			self.picar.always_forward(75)
+			self.delay_stop()
+		elif order == "backward":
+			self.picar.always_backward(75)
+			self.delay_stop()
+		elif order == "forward left":
+			self.picar.set_dir_servo_angle(-30)
+			self.picar.forward(75)
+			self.delay_stop()
+		elif order == "forward right":
+			self.picar.set_dir_servo_angle(30)
+			self.picar.forward(75)
+			self.delay_stop()
+		elif order == "backward left":
+			self.picar.set_dir_servo_angle(-30)
+			self.picar.backward(75)
+			self.delay_stop()
+		elif order == "backward right":
+			self.picar.set_dir_servo_angle(30)
+			self.picar.backward(75)
+			self.delay_stop()
+		elif order == 'stop':
+			self.picar.stop()
+
+
 	def parallel_park(self, order = "left"):
 		if order == "left":
-            self.picar.set_dir_servo_angle(0)
-            self.picar.forward(75)
-            self.picar.set_dir_servo_angle(-30)
-            delay(1000)
-            self.picar.set_dir_servo_angle(30)
-            self.delay_stop()
+			self.picar.set_dir_servo_angle(0)
+			self.picar.forward(75)
+			self.picar.set_dir_servo_angle(-30)
+			delay(1000)
+			self.picar.set_dir_servo_angle(30)
+			self.delay_stop()
 
-        elif order == "right":
-            self.picar.set_dir_servo_angle(0)
-            self.picar.forward(75)
-            self.picar.set_dir_servo_angle(30)
-            delay(1000)
-            self.picar.set_dir_servo_angle(-30)
-            self.delay_stop()
+		elif order == "right":
+			self.picar.set_dir_servo_angle(0)
+			self.picar.forward(75)
+			self.picar.set_dir_servo_angle(30)
+			delay(1000)
+			self.picar.set_dir_servo_angle(-30)
+			self.delay_stop()
 
-    def kturning(self, order=left):
-        if order == "left":
-            self.Moveit("forward left")
+	def kturning(self, order=left):
+		if order == "left":
+			self.Moveit("forward left")
 
-            self.picar.set_dir_servo_angle(30)
-            self.picar.backward(50)
-            self.delay_stop()
+			self.picar.set_dir_servo_angle(30)
+			self.picar.backward(50)
+			self.delay_stop()
 
-            self.picar.set_dir_servo_angle(-20)
-            self.picar.forward(75)
-            delay(1000)
-            self.picar.set_dir_servo_angle(0)
-            self.delay_stop()
+			self.picar.set_dir_servo_angle(-20)
+			self.picar.forward(75)
+			delay(1000)
+			self.picar.set_dir_servo_angle(0)
+			self.delay_stop()
+			
+		elif order == "right":
+			self.Moveit("forward right")
 
-        elif order == "right":
+			self.picar.set_dir_servo_angle(-30)
+			self.picar.backward(50)
+			self.delay_stop()
 
-            self.Moveit("forward right")
-
-            self.picar.set_dir_servo_angle(-30)
-            self.picar.backward(50)
-            self.delay_stop()
-
-            self.picar.set_dir_servo_angle(20)
-            self.picar.forward(75)
-            delay(1000)
-            self.picar.set_dir_servo_angle(0)
-            self.delay_stop()
-
+			self.picar.set_dir_servo_angle(20)
+			self.picar.forward(75)
+			delay(1000)
+			self.picar.set_dir_servo_angle(0)
+			self.delay_stop()
 
 if __name__ == "__main__":
-
-    manuver = Manuver()
-    actions = ["forward right", "backward right", "forward left", "backward left", \
-            				"forward", "backward"]
-    dirs = ["left", "right"]
-    while True:
+	manuver = Manuver()
+	actions = ["forward right", "backward right", "forward left", "backward left","forward", "backward"]
+	dirs = ["left", "right"]
+	while True:
         usr_input = int(input(
             "Choose maneuver: \n1] move in direction \n2] park in direction \n3] k-turn \n4] stop \n5] exit \n"))
         manuver.emergency_stop()
