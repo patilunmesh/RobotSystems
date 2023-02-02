e9a898501e07997aec09ae9cbc8118b0b17bb637
patilunmesh <patilunmeshrao@gmail.com>
2023-02-02 11:06:36 -1000

    greyscale module hardware test

---
+++ example/greyscale.py
@@ -0,0 +1,56 @@
+from robot_hat import *
+import numpy as np
+from enum import IntEnum
+from typing import List
+from picarx import Picarx
+from time import sleep
+
+SensorOutput = List[int]
+PERIOD = 4095
+PRESCALER = 10
+
+class Sensor(object):
+    def __init__(self, pin0, pin1, pin2):
+        self.chn_0 = ADC(pin0)
+        self.chn_1 = ADC(pin1)
+        self.chn_2 = ADC(pin2)
+
+    def get_grayscale_data(self):
+        adc_value_list = []
+        adc_value_list.append(self.chn_0.read())
+        adc_value_list.append(self.chn_1.read())
+        adc_value_list.append(self.chn_2.read())
+        return adc_value_list
+
+class Polarity(IntEnum):
+    Dark = -1
+    Light = +1
+
+
+class Interpreter:
+
+    def __init__(self,
+                 sensitivity: float = 1e-0,
+                 polarity: Polarity = Polarity.Dark) -> None:
+
+        self.sensitivity = sensitivity
+        self.polarity = polarity
+
+    def process(self, sensor_values: SensorOutput) -> float:
+
+        # caluclate difference
+        mid = len(sensor_values) // 2
+        delta = np.array(sensor_values[:mid+1]) - np.array(sensor_values[mid:]) * self.polarity
+        delta[mid:] *= -1  
+        return np.clip(np.mean(delta) * self.sensitivity, -1, 1)
+
+class Controller:
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.scale = 1
+        self.car = Picarx()
+        self.value = 0
+        self.greyscale_out = 8
+    def steer(self, value) -> float:
+        print("Grayscale::Controller::steer() steering.")
+        self.car.set_dir_servo_angle(-1*self.scale* value * self.greyscale_out)
--- example/line_fol.py
+++ example/line_fol.py
@@ -1,16 +1,20 @@
 from picarx import Picarx
-import Picarx.sensors.grayscale as Grayscale
+from greyscale import Sensor, Interpreter, Controller
 
 if __name__ == '__main__':
 
-    print("\ntesting grayscale sensor. Using ultrasonic sensor for breaks.")
+    print("\ntesting grayscale sensor.")
 
     # test car
     car = Picarx()
 
     # grayscale sensor, interpreter and controller
-    sensor = Grayscale.Sensor(['A0', 'A1', 'A2'])
-    interpreter = Grayscale.Interpreter()
-    controller = Grayscale.Controller(None, car)
+    sensor = Sensor('A0', 'A1', 'A2')
+    interpreter = Interpreter()
+    controller = Controller(None, car)
 
-    
+    data  = sensor.get_grayscale_data()
+    print(data)
+    ret = interpreter.process(data)
+    print(ret)
+    controller.steer(ret)
