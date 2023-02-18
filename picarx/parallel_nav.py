d2f9e15bd93e929780ce2770e8b2cb6b6df32da6
patilunmesh <patilunmeshrao@gmail.com>
2023-02-18 12:27:08 -0800

    tested

--- picarx/parallel_nav.py
+++ picarx/parallel_nav.py
@@ -1,3 +1,4 @@
+import sys
 sys.path.append('')
 try:
     from picarx import Picarx
@@ -15,8 +16,19 @@ from rossros.rossros import Bus, Producer, ConsumerProducer, Consumer, runConcur
 
 if __name__ == '__main__':
 
-    print("\ngrayscale sensor for line folloing and ultrasonic sensor for breaks.")
+    print("grayscale sensor for line folloing and ultrasonic sensor for breaks.")
     car = Picarx()
+
+    ########################## Timer for killing threads #########################################
+    
+    timeout = Bus(initial_message= False, name="timeout")
+    killingTimer = Timer(output_buses = timeout,  # buses that receive the countdown value
+                         duration=5,  # how many seconds the timer should run for (0 is forever)
+                         delay=0,  # how many seconds to sleep for between checking time
+                         termination_buses=Bus(False, "Default timer termination bus"),
+                         name="killbus")
+
+
     ########################## Sensor bus generation section #############################
 
     # creating grayscale sensor, interpreter and controller and busses
@@ -36,26 +48,40 @@ if __name__ == '__main__':
 
     # grayscale producer, control and consumer
     grayscale_producer = Producer(sensor.read, output_busses=gray_input_bus,delay=0.5,name="grayscale_producer")
-    grayscale_control = ConsumerProducer(interpreter.process,input_busses=gray_in_bus,output_busses=gray_out_bus,delay=1.0,
-                                         name="grayscale_control")
-    grayscale_consumer = Consumer(controller.steer,input_busses=gray_out_bus,delay=1.0,name="grayscale_consumer")
+    grayscale_control = ConsumerProducer(interpreter.process,
+                                         input_busses=gray_in_bus,output_busses=gray_out_bus,
+                                         delay=1.0,
+                                         termination_buses = timeout,
+                                         name="grayscale_control"
+                                         )
+    grayscale_consumer = Consumer(controller.steer,
+                                  input_busses=gray_out_bus, 
+                                  delay=1.0,
+                                  termination_buses = timeout,
+                                  name="grayscale_consumer")
 
     
     
     ultrasonic_producer = Producer(ultrasonic.read,
-                                   output_busses=ultrasonic_input, delay=0.3,
+                                   output_busses=ultrasonic_input,
+                                   delay=0.3,
+                                   termination_buses = timeout,
                                    name="ultrasonic_producer")
 
     ultrasonic_control = ConsumerProducer(ultrasonic.process,
                                           input_busses=ultrasonic_input,
-                                          output_busses=ultrasonic_output, delay=0.5,
+                                          output_busses=ultrasonic_output, 
+                                          delay=0.5,
+                                          termination_buses = timeout,
                                           name="ultrasonic_control")
     ultrasonic_consumer = Consumer(ultrasonic.control,
-                                   input_busses=ultrasonic_output, delay=0.5,
+                                   input_busses=ultrasonic_output, 
+                                   delay=0.5,
+                                   termination_buses = timeout,
                                    name="ultrasonic_consumer")
 
     ############################################### Concurrent execution ########################
-    
+
     runConcurrently([
         # grayscale
         grayscale_producer,
